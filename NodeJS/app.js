dd78bf2a0374e1e4b8b83e240a442abd30ae99f5
NarendraDhamecha <nikhildhamecha19@gmail.com>
2023-05-22 13:14:27 +0530

    Learning Request / Response

--- NodeJS/app.js
+++ NodeJS/app.js
@@ -1,7 +1,29 @@
-const http = require('http');
+const http = require("http");
 
 const server = http.createServer((req, res) => {
-    console.log('nikhil');
-})
+  if (req.url === "/home") {
+    res.setHeader("Content-Type", "text/html");
+    res.write("<html>");
+    res.write("<h1>Welcome Home</h1>")
+    res.write("</html>");
+    return res.end();
+  }
+
+  if (req.url === "/about") {
+    res.setHeader("Content-Type", "text/html");
+    res.write("<html>");
+    res.write("<h1>Welcome to About Us Page</h1>")
+    res.write("</html>");
+    return res.end();
+  }
+
+  if (req.url === "/node") {
+    res.setHeader("Content-Type", "text/html");
+    res.write("<html>");
+    res.write("<h1>Welcome to my Node Js project</h1>")
+    res.write("</html>");
+    return res.end();
+  }
+});
 
 server.listen(4000);
