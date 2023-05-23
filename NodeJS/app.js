3386f94655385e322553eaa5c2a448da212f3691
NarendraDhamecha <nikhildhamecha19@gmail.com>
2023-05-23 14:25:53 +0530

    writing and reading from a file

--- NodeJS/app.js
+++ NodeJS/app.js
@@ -1,29 +1,38 @@
 const http = require("http");
+const fs = require("fs");
 
 const server = http.createServer((req, res) => {
-  if (req.url === "/home") {
-    res.setHeader("Content-Type", "text/html");
-    res.write("<html>");
-    res.write("<h1>Welcome Home</h1>")
-    res.write("</html>");
-    return res.end();
+  const url = req.url;
+  const method = req.method;
+
+  if (url === "/") {
+    fs.readFile("message.txt", { encoding: "utf-8" }, (err, data) => {
+      res.write("<html>");
+      res.write(`<body>${data}</body>`);
+      res.write(
+        "<body><form action='/message' method='POST'><input type='text' name='message'><button type='submit'>Send</button></form></body>"
+      );
+      res.write("</html>");
+      return res.end();
+    });
   }
 
-  if (req.url === "/about") {
-    res.setHeader("Content-Type", "text/html");
-    res.write("<html>");
-    res.write("<h1>Welcome to About Us Page</h1>")
-    res.write("</html>");
-    return res.end();
-  }
+  if (url === "/message" && method === "POST") {
+    const body = [];
+    req.on("data", (chunk) => {
+      body.push(chunk);
+    });
 
-  if (req.url === "/node") {
-    res.setHeader("Content-Type", "text/html");
-    res.write("<html>");
-    res.write("<h1>Welcome to my Node Js project</h1>")
-    res.write("</html>");
-    return res.end();
+    req.on("end", () => {
+      const parsedBody = Buffer.concat(body).toString();
+      const message = parsedBody.split("=")[1];
+      fs.writeFile("message.txt", message, (err) => {
+        res.statusCode = 302;
+        res.setHeader("Location", "/");
+        return res.end();
+      });
+    });
   }
 });
 
-server.listen(4000);
+server.listen(3000);
