b2d9eb22eb27859f94dcfccab9a85691bebebdd3
NarendraDhamecha <nikhildhamecha19@gmail.com>
2023-05-30 17:13:03 +0530

    intro to middlewares and body-parser

--- app.js
+++ app.js
@@ -1,17 +1,24 @@
 const http = require("http");
 const express = require("express");
+const bodyParser = require("body-parser");
 
-const app = express(); 
+const app = express();
 
-app.use((req, res, next) => {
-    console.log("In the middleware");
-    next();
-})
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use((req, res, next) => {
-    console.log("In another middleware")
-    res.send('<h1> hello to node js </h1>');
-})
+app.use("/add-product", (req, res, next) => {
+  res.send(
+    '<form action="/product" method="POST"><input type="text" name="title"/><input type="number" name="age"/><button type="submit">add product</button></form>'
+  );
+});
 
+app.post("/product", (req, res, next) => {
+  console.log(req.body);
+  res.redirect("/");
+});
+
+app.use("/", (req, res, next) => {
+  res.send("<h1>Hello from Express</h1>");
+});
 
 app.listen(3000);
